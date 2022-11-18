07b0de4610e903e44d00ba06fb988a115c6d59f4
SivaPullaiah <mangalisivapullaiah@gmail.com>
2022-11-18 11:25:30 +0530

    Django hello world project is my first django project

---
+++ Django ex/Environments/hworld/hello_world_project/my_app/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse  '''this was inserted'''
+def index(request):                   '''this was inserted'''
+    return HttpResponse('Hello World!')  '''this was inserted'''
+# Create your views here.
